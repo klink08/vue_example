6c0c3b1103e5cbfa11ed33174d201f925cf0a59a
Klink <klink008@gmail.com>
2025-05-23 11:06:13 -0400

    Adding in dashboard component.

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useUserStore } from "@/stores/user.js";
+
+const userStore = useUserStore();
+</script>
+
+<template>
+  <main>
+    <div>TESTING</div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
